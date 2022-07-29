2b83b4a07bb26c3f5302e3e6cbe98d34b4cf97fb
Harikrishna Gouramoni (IC) <hgouramoni@tapestry.com>
2022-07-29 21:22:48 +0530

    updated few changes for testing

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <br />
-      Welcome to React Applciation !!
+      Welcome to React Application !!
       This app is deployed in AWS - AppSync service !
+      <br />
+
+      This app has been updated now...
     </div>
   );
 }
